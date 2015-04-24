e19882172ec7e5ee74ad2d99dfde561bc78447cf
Stanislaw Klekot <dozzie@jarowit.net>
2015-04-24 11:36:48 +0200

    Added cflags and ldflags to compilation of the C module.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages, Extension
+import commands
+
+cflags = commands.getoutput('pkg-config --cflags lognorm').strip().split()
+ldflags = commands.getoutput('pkg-config --libs lognorm').strip().split()
 
 setup(
     name = "liblognorm",
     version = "0.0.0",
     description = "fast log normalization library",
     ext_modules = [
-        Extension('liblognorm', sources = ['c_lib/lognorm.c']),
+        Extension(
+            'liblognorm',
+            sources = ['c_lib/lognorm.c'],
+            extra_compile_args = cflags,
+            extra_link_args = ldflags,
+        ),
     ],
 )
